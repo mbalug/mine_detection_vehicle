1e10b0d6dfda59d96c6c6c653b02f4c009ec2250
home <mbalug@gmail.com>
2018-12-13 19:08:29 +0100

    added serial string parsing and wheels controlling

--- src/main.c
+++ src/main.c
@@ -1,26 +1,20 @@
 #include <stm32f4xx.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "../lib/delay.h"
+#include "../lib/tm_stm32f4_spi.h"
+#include "../lib/tm_stm32f4_dac.h"
 
 #define LED_PIN GPIO_Pin_14
-#define SPI1_SS GPIO_Pin_4
-
-#define BLDC_ENABLE GPIO_Pin_1
-
-
-uint32_t multiplier;
-
 
 void Init_USART2()
 {
 
-    /* Bit configuration structure for GPIOA PIN9 and PIN10 */
-    GPIO_InitTypeDef GPIO_InitStruct;
-    /* Enalbe clock for USART1, AFIO and GPIOA */
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
 
-
+    GPIO_InitTypeDef GPIO_InitStruct;
     GPIO_PinAFConfig(GPIOD, GPIO_PinSource5, GPIO_AF_USART2);
     GPIO_PinAFConfig(GPIOD, GPIO_PinSource6, GPIO_AF_USART2);
     // Initialize pins as alternating function
@@ -33,8 +27,6 @@ void Init_USART2()
 
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
 
-    USART_Cmd(USART2, ENABLE);
-
 	USART_InitTypeDef USART_InitStructure;
 	USART_InitStructure.USART_BaudRate = 9600;
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
@@ -42,26 +34,20 @@ void Init_USART2()
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 	USART_InitStructure.USART_Parity = USART_Parity_No ;
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    /* Configure USART1 */
+    USART_Cmd(USART2, ENABLE);
+
     USART_Init(USART2, &USART_InitStructure);
     /* Enable RXNE interrupt */
     USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
-
-
-	// Enable interrupt fromUSART2(NVIC level)
 	NVIC_EnableIRQ(USART2_IRQn);
 }
 
 void Init_USART6()
 {
 
-    /* Bit configuration structure for GPIOA PIN9 and PIN10 */
-    GPIO_InitTypeDef GPIO_InitStruct;
-    /* Enalbe clock for USART1, AFIO and GPIOA */
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 
-
-
+    GPIO_InitTypeDef GPIO_InitStruct;
     GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_USART6);
     GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_USART6);
     GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
@@ -87,78 +73,53 @@ void Init_USART6()
     /* Enable RXNE interrupt */
     USART_ITConfig(USART6, USART_IT_RXNE, ENABLE);
 
-
-	// Enable interrupt fromUSART2(NVIC level)
-	NVIC_EnableIRQ(USART6_IRQn);
+    NVIC_InitTypeDef NVIC_InitStruct;
+    NVIC_InitStruct.NVIC_IRQChannel = USART6_IRQn;
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+    NVIC_Init(&NVIC_InitStruct);
 }
 
-void Init_SPI1()
-{
-	/*
-	 * PB3 -> SCK
-	 * PB4 -> MISO
-	 * PB5 -> MOSI
-	 * PA4 -> SS
-	 */
 
-	GPIO_InitTypeDef  GPIO_InitStructure;
+void setDac1(int16_t dac)
+{
+
+}
+
+void initDAC()
+{
+	// GPIO setup PA4
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-    // Initialize pins as alternating function
-    GPIO_InitStruct.GPIO_Pin = SPI1_SS;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AN;
     GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
     GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-    GPIO_InitTypeDef GPIO_InitStruct;
-    GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF_SPI1);
-    GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, GPIO_AF_SPI1);
-    GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, GPIO_AF_SPI1);
-    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-    GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
-    SPI_Cmd(SPI1, ENABLE);
-    SPI_InitTypeDef SPI_InitStructure;
-    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
-    SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
-    SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
-    SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
-    SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
-    SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
-    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
-    SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
-    SPI_Init(SPI1, &SPI_InitStructure);
-
+//
+//	// DAC 1 Setup
+//	RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC, ENABLE);
+//	DAC_InitTypeDef DAC1_InitStructure;
+//	DAC_Init(DAC_Channel_1, &DAC1_InitStructure);
+//	DAC_Cmd(DAC_Channel_1 ,ENABLE);
+//
+//	DAC_InitTypeDef DAC2_InitStructure;
+//	DAC_Init(DAC_Channel_2, &DAC2_InitStructure);
+//	DAC_Cmd(DAC_Channel_2 ,ENABLE);
+//
+//    DAC_TypeDef DAC_InitStruct;
+//    DAC_InitStruct.CR |= DAC_CR_EN1 | DAC_CR_EN2;
+//    DAC_InitStruct.DHR12R1 = 0;
+//    DAC_InitStruct.DHR12R2 = 0;
+//    DAC_Init(DAC_Channel_1|DAC_Channel_2, &DAC_InitStruct);
+    TM_DAC_Init(TM_DAC1);
+    TM_DAC_Init(TM_DAC2);
 }
 
-volatile char recieveBuffer[2000];
+char recieveBuffer[2000];
 volatile int i = 0;
 
-void USART2_IRQHandler()
-{
-	//USART_SendData(USART2, 'T');
-	// Do Something    /* RXNE handler */
-	if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
-	{
-		recieveBuffer[i] = (char)USART_ReceiveData(USART2);
-		if(recieveBuffer[i] == '\r')
-		{
-			recieveBuffer[i]=0;
-			printf("%s\n",recieveBuffer);
-			i = 0;
-		}
-		else
-		{
-			i++;
-		}
-
-	}
-}
 
 void UU_PutChar(USART_TypeDef* USARTx, uint8_t ch)
 {
@@ -174,84 +135,144 @@ void UU_PutString(USART_TypeDef* USARTx, char * str)
     str++;
   }
 }
+void USART2_IRQHandler()
+{
+//	if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
+//	{
+//		recieveBuffer[i] = (char)USART_ReceiveData(USART2);
+//		if(recieveBuffer[i] == '\r')
+//		{
+//			recieveBuffer[i]=0;
+//			printf("%s\n",recieveBuffer);
+//			i = 0;
+//
+//			UU_PutString(USART2, "OVO JE KURAC");
+//
+//		}
+//		else
+//		{
+//			i++;
+//			printf("Charrcved");
+//		}
+//
+//	}
+}
 
-char recieveBuffer1[4000];
+void messageHandler(char buffer[20])
+{
+	if(buffer[0] == 'F')
+	{
+		if(buffer[1]=='L')
+		{
+			char left[10];
+			strncpy(left, buffer + 2, strlen(buffer)-1);
+			uint16_t left_speed=atoi(left);
+			printf("FL: %d\n",left_speed);
+			TM_DAC_SetValue(TM_DAC2, left_speed);
+			//DAC_SetChannel2Data(DAC_Align_12b_L, 4095 - left_speed);
+			//set direction
+			//set dac
+		}
+		if(buffer[1]=='R')
+		{
+			char right[10];
+			strncpy(right, buffer + 2, strlen(buffer)-1);
+			uint16_t right_speed=atoi(right);
+			printf("FR: %d\n",right_speed);
+			TM_DAC_SetValue(TM_DAC1, 4095 - right_speed);
+			//DAC_SetChannel1Data(DAC_Align_12b_R, 4095 - right_speed);
+		}
+	}
+	else if(buffer[0] == 'R')
+	{
+		if(buffer[1]=='L')
+		{
+			char left[10];
+			strncpy(left, buffer + 2, strlen(buffer)-1);
+			uint16_t left_speed=atoi(left);
+			printf("RL: %d\n",left_speed);
+		}
+		if(buffer[1]=='R')
+		{
+			char right[10];
+			strncpy(right, buffer + 2, strlen(buffer)-1);
+			uint16_t right_speed=atoi(right);
+			printf("RR: %d\n",right_speed);
+		}
+	}
+
+}
+
+
+char recieveBuffer1[1000];
 int n = 0;
 int rows_count = 0;
 void USART6_IRQHandler()
 {
 	//USART_SendData(USART2, 'T');
 	// Do Something    /* RXNE handler */
+//	if(USART_GetITStatus(USART6, USART_IT_RXNE) != RESET)
+//	{
+//			recieveBuffer1[n] = (char)USART_ReceiveData(USART6);
+//			if(recieveBuffer1[n] == '\n')
+//			{
+//
+//				if(rows_count>10)
+//				{
+//					recieveBuffer1[n+1] = '\r';
+//					recieveBuffer1[n+2] = 0;
+//					/*for(int i = 0; i<n;i++)
+//					{
+//						USART_SendData(USART2,(uint16_t)recieveBuffer1[i]);
+//
+//					}*/
+//					printf("********************************/////   %d\n\n\n",n);
+//					printf("%s\n\r",recieveBuffer1);
+//					n = 0;
+//					rows_count=0;
+//					UU_PutString(USART2, recieveBuffer1);
+//				}
+//				else
+//				{
+//					rows_count++;
+//					n++;
+//				}
+//			}
+//			else
+//			{
+//				n++;
+//			}
+//	}
+//
 	if(USART_GetITStatus(USART6, USART_IT_RXNE) != RESET)
 	{
-			recieveBuffer1[n] = (char)USART_ReceiveData(USART6);
-			if(recieveBuffer1[n] == '\n')
-			{
-
-				if(rows_count>10)
-				{
-					recieveBuffer1[n+1] = "\r";
-					recieveBuffer1[n+2] = 0;
-					/*for(int i = 0; i<n;i++)
-					{
-						USART_SendData(USART2,(uint16_t)recieveBuffer1[i]);
-
-					}*/
-					printf("********************************/////   %d\n\n\n",n);
-					printf("%s\n\r",recieveBuffer1);
-					n = 0;
-					rows_count=0;
-					UU_PutString(USART2, recieveBuffer1);
-				}
-				else
-				{
-					rows_count++;
-					n++;
-				}
-			}
-			else
-			{
-				n++;
-			}
-
-//
+		recieveBuffer[i] = (char)USART_ReceiveData(USART6);
+		if(recieveBuffer[i] == '\n')
+		{
+			recieveBuffer[i]=0;
+			i = 0;
+			messageHandler(recieveBuffer);
+//			printf("%s\n",recieveBuffer);
+//			//UU_PutString(USART6, "WORLD\n");
+//			if(strstr(recieveBuffer, "Stre:") != NULL) {
+//			    char *sub_str = strstr(recieveBuffer, "Stre:");
+//			    char num[100];
+//			    strncpy(num, sub_str + 6, strlen(sub_str)-1);
+//			    //UU_PutString(USART6, sub_str);
+//			    // (num[0]-48)*100+
+//			    uint8_t pwmDuty=atoi(num);
+//			    printf("BROJ :%d\n", pwmDuty);
+//			    DAC_SetChannel1Data(DAC_Align_8b_R, 255-(pwmDuty*2));
+			//}
+		}
+		else
+		{
+			i++;
+		}
 
 	}
 }
 
-
-void init_BLDC_Controller()
-{
-	GPIO_InitTypeDef  GPIO_InitStructure;
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-
-
-    GPIO_InitStruct.GPIO_Pin = BLDC_ENABLE;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_Init(GPIOB, &GPIO_InitStruct);
-    GPIO_SetBits(GPIOB, BLDC_ENABLE);
-
-}
-
-void send_BLDC_data(uint8_t address, uint8_t dataH, uint8_t dataL)
-{
-	GPIO_SetBits(GPIOA, SPI1_SS);
-	SPI_I2S_SendData(SPI1, address);
-	SPI_I2S_SendData(SPI1, dataH);
-	SPI_I2S_SendData(SPI1, dataL);
-	GPIO_ResetBits(GPIOA, SPI1_SS);
-}
-
-uint16_t get_BLDC_data(uint8_t address)
-{
-	GPIO_SetBits(GPIOA, SPI1_SS);
-	SPI_I2S_SendData(SPI1, address);
-	uint16_t data = SPI_I2S_ReceiveData(SPI1);
-	GPIO_ResetBits(GPIOA, SPI1_SS);
-	return data;
-}
 
 int main() {
 
@@ -259,22 +280,15 @@ int main() {
 
 	TM_Delay_Init();
 
-	Init_USART2();
+	//Init_USART2();
 
 	Init_USART6();
 
-	Init_SPI1();
-
-	init_BLDC_Controller();
-
 	/* GPIOG Peripheral clock enable */
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-
 	GPIO_InitTypeDef  GPIO_InitStructure;
 
-	/* Configure PG6 and PG8 in output pushpull mode */
 	GPIO_InitStructure.GPIO_Pin = LED_PIN;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
@@ -282,29 +296,42 @@ int main() {
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 
 	GPIO_InitTypeDef  GPIO_InitStructureIn;
-	GPIO_InitStructureIn.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStructureIn.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_2;
 	GPIO_InitStructureIn.GPIO_Mode = GPIO_Mode_IN;
 	GPIO_InitStructureIn.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
 	GPIO_Init(GPIOC, &GPIO_InitStructureIn);
 
-	printf("Ovo je zaista kul stvar");
+	initDAC();
 
-
-
+	TM_DAC_SetValue(TM_DAC1, 4095);
+	TM_DAC_SetValue(TM_DAC2, 0);
 
 	while (1)
 	{
 		if(GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_13)==1)
 		{
-			GPIOB->BSRRL = LED_PIN;
-			UU_PutString(USART2, "Mali Mario\n\r");
+			GPIO_ResetBits(GPIOB, LED_PIN);
+
+		    TM_DelayMillis(1000);
+
+			UU_PutString(USART6, "Mali Mario\n\r");
+//			//uint8_t data[3] = {0x00,0x00,0x01};
+//			uint16_t rcv;
+//			send_BLDC_data(0x00,0x00,0x01);
+//			//SPI1_Transmit(data, &rcv, 3);
+//
+//			TM_DelayMillis(1);
+//
+//			//uint8_t data1[1] = {0x80};
+//			uint16_t data = get_BLDC_data(0x80);
+//			printf("MSG 0x00 %x", rcv);
 
 		}
 		else
 		{
-
-			GPIOB->BSRRH = LED_PIN;
+			GPIO_SetBits(GPIOB, LED_PIN);
+			TM_DelayMicros(25);
 		}
 
 	}
